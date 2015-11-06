eda464a8e0e85e1c5e7eb711e46be3ae9f71ea03
Ethan Mosbaugh <emosbaugh@gmail.com>
2015-11-06 09:46:38 -0800

    latest aws sdk

--- mail/mail.go
+++ mail/mail.go
@@ -1,13 +1,10 @@
 package mail
 
 import (
-	"encoding/json"
 	"errors"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	_ "github.com/aws/aws-sdk-go/internal/endpoints"
-	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/replicatedcom/saaskit/common"
 )
 
 func SendMail(recipients []string, template string, subject string, context map[string]interface{}) error {
@@ -32,56 +29,11 @@ func sendMail(action string, recipients []string, template string, subject strin
 		Context:    context,
 	}
 
-	return deliverSqsMessage(action, request)
-}
-
-func deliverSqsMessage(action string, payload interface{}) error {
-	b, err := json.Marshal(payload)
-	if err != nil {
-		return err
-	}
-
-	if len(os.Getenv("AWS_ACCESS_KEY_ID")) == 0 {
-		err := errors.New("AWS_ACCESS_KEY_ID must be set before starting")
-		return err
-	}
-	if len(os.Getenv("AWS_SECRET_ACCESS_KEY")) == 0 {
-		err := errors.New("AWS_SECRET_ACCESS_KEY must be set before starting")
-		return err
-	}
-
-	client := sqs.New(&aws.Config{Region: aws.String("us-east-1")})
-
 	queueName := os.Getenv("AWS_SQS_MAIL_QUEUENAME")
 	if len(queueName) == 0 {
 		err := errors.New("AWS_SQS_MAIL_QUEUENAME must be set before starting")
 		return err
 	}
 
-	getQueueUrlRequest := &sqs.GetQueueUrlInput{
-		QueueName:              aws.String(queueName),
-		QueueOwnerAWSAccountId: aws.String("323305220431"),
-	}
-	getQueueUrlOutput, err := client.GetQueueUrl(getQueueUrlRequest)
-	if err != nil {
-		return err
-	}
-
-	sendMessageInput := &sqs.SendMessageInput{
-		MessageBody: aws.String(string(b[:])),
-		QueueUrl:    getQueueUrlOutput.QueueUrl,
-		MessageAttributes: map[string]*sqs.MessageAttributeValue{
-			"Key": {
-				DataType:    aws.String("String"),
-				StringValue: aws.String(action),
-			},
-		},
-	}
-
-	_, err = client.SendMessage(sendMessageInput)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return common.SQSDeliverMessage(queueName, action, request)
 }
