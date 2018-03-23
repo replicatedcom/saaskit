3be7ed49bc8369d18bd50816deafd2ba427b90ab
Ethan Mosbaugh <ethan@replicated.com>
2018-03-22 20:41:41 -0700

    log

--- log/init.go
+++ log/init.go
@@ -1,7 +1,15 @@
 package log
 
+import (
+	"github.com/replicatedcom/saaskit/param"
+)
+
 func Init(logOpts *LogOptions, mailLogOpts *MailLogOptions, slackLogOpts *SlackLogOptions) {
 	InitLog(logOpts)
 	InitMail(mailLogOpts)
 	InitSlack(slackLogOpts)
+
+	if param.Lookup("PROJECT_NAME", "", false) == "" {
+		Infof("Environment variable PROJECT_NAME not set")
+	}
 }
