015dfe1267024bfdf686f46f87ebf3f152eb1b89
kylerisse <kylerisse@users.noreply.github.com>
2021-05-21 14:03:27 -0700

    add datadog package

--- go.mod
+++ go.mod
@@ -3,16 +3,26 @@ module github.com/kylerisse/saaskit
 go 1.16
 
 require (
+	github.com/Microsoft/go-winio v0.5.0 // indirect
 	github.com/aws/aws-sdk-go v1.38.45
 	github.com/bitly/go-simplejson v0.5.0 // indirect
 	github.com/bugsnag/bugsnag-go v2.1.1+incompatible
 	github.com/bugsnag/panicwrap v1.3.2 // indirect
 	github.com/gin-gonic/gin v1.7.1
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/johntdyer/slack-go v0.0.0-20180213144715-95fac1160b22
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/lib/pq v1.10.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.7 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/philhofer/fwd v1.1.1 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/replicatedcom/saaskit v0.0.0-20180719175845-249c0d6c71b2
 	github.com/sirupsen/logrus v1.8.1
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
+	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/DataDog/dd-trace-go.v1 v1.31.0
 )
---
+++ tracing/datadog/datadog.go
@@ -0,0 +1,107 @@
+package datadog
+
+import (
+	"context"
+	"database/sql"
+	"database/sql/driver"
+	"os"
+	"runtime"
+
+	"github.com/gin-gonic/gin"
+	sqltrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/database/sql"
+	gintrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gin-gonic/gin"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+)
+
+var ddIsEnabled bool
+
+func init() {
+	ddIsEnabled = os.Getenv("USE_DATADOG_APM") == "true"
+}
+
+// DDFinishable confirms ddtrace to Finishable
+type DDFinishable interface {
+	Finish(...ddtrace.FinishOption)
+}
+
+// Finishable is a wrapped ddtrace, which can Finish
+type Finishable struct {
+	toFinish DDFinishable
+}
+
+// Finish a Finishable, safe if nil
+func (f *Finishable) Finish() {
+	if f.toFinish != nil {
+		f.toFinish.Finish()
+	}
+}
+
+// StartSpanFromGin takes a gin context and returns a wrapped Span plus
+// the span's context. If Datadog APM isn't enabled it simply returns
+// a wrapped Nil, which is safe to Finish() and the gin context's HTTP
+// request context
+func StartSpanFromGin(c *gin.Context) (*Finishable, context.Context) {
+	if datadogEnabled() {
+		var spanName string
+		rctx := c.Request.Context()
+		pc, _, _, ok := runtime.Caller(1)
+		if !ok {
+			spanName = "undefined"
+		} else {
+			spanName = runtime.FuncForPC(pc).Name()
+		}
+		span, ctx := tracer.StartSpanFromContext(rctx, spanName)
+		return &Finishable{
+			toFinish: span,
+		}, ctx
+	} else {
+		return &Finishable{nil}, c.Request.Context()
+	}
+}
+
+// GinMiddleware wraps gin tracer's middleware
+// which already plays nice when Datadog APM is disabled
+func GinMiddleware(service string) gin.HandlerFunc {
+	return gintrace.Middleware(service)
+}
+
+// StartTracer starts the tracer, taking a Service name and version
+func StartTracer(serviceName, version string) {
+	if datadogEnabled() {
+		tracer.Start(
+			tracer.WithService(serviceName),
+			tracer.WithServiceVersion(version),
+		)
+	}
+}
+
+// StopTracer stops the tracer, typically called with defer in the same
+// scope as StartTracer.
+func StopTracer() {
+	if datadogEnabled() {
+		tracer.Stop()
+	}
+}
+
+// RegisterSQL Registers the SQL driver with a service name in Datadog
+// when Datadog APM is enabled
+func RegisterSQL(driverName string, driver driver.Driver, dbName string) {
+	if datadogEnabled() {
+		sqltrace.Register(driverName, driver, sqltrace.WithServiceName(dbName))
+	}
+}
+
+// OpenSQL Opens a DB with the Datadog tracer when Datadog APM
+// is enabled. Otherwise it uses the standrd library's sql.Open
+func OpenSQL(driverName, dataSourceName string) (*sql.DB, error) {
+	if datadogEnabled() {
+		return sqltrace.Open(driverName, dataSourceName)
+	} else {
+		return sql.Open(driverName, dataSourceName)
+	}
+}
+
+func datadogEnabled() bool {
+	return ddIsEnabled
+}
