ab7127e27fbfe56eff00f54d20c2b932a64224aa
divolgin <dmitriy@replicated.com>
2025-03-14 15:11:26 -0700

    Remove Slack logger

--- log/init.go
+++ log/init.go
@@ -4,10 +4,9 @@ import (
 	"github.com/replicatedcom/saaskit/param"
 )
 
-func Init(logOpts *LogOptions, mailLogOpts *MailLogOptions, slackLogOpts *SlackLogOptions) {
+func Init(logOpts *LogOptions, mailLogOpts *MailLogOptions) {
 	InitLog(logOpts)
 	InitMail(mailLogOpts)
-	InitSlack(slackLogOpts)
 
 	if param.Lookup("PROJECT_NAME", "", false) == "" {
 		Infof("Environment variable PROJECT_NAME not set")
